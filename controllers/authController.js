9141c041633e7ca42cb67d5ef4e676455fe1b0cd
mayrars <mayra203@gmail.com>
2024-11-05 07:13:35 -0600

    created singin

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,10 +1,11 @@
-const { singupSchema } = require("../middlewares/validator");
+const jwt = require("jsonwebtoken")
+const { singupSchema, signupSchema } = require("../middlewares/validator");
 const User = require("../models/usersModel");
-const { doHash } = require("../utils/hashing");
+const { doHash, doHashValidation } = require("../utils/hashing");
 exports.signup = async (req, res) => {
     const {email, password} = req.body;
     try{
-        const {error, value} = singupSchema.validate({email, password})
+        const {error, value} = signupSchema.validate({email, password})
 
         if(error){
             return res.status(401).json({success: false, message: error.details[0].message})
@@ -25,6 +26,56 @@ exports.signup = async (req, res) => {
         result.password = undefined;
         res.status(201).json({success: true, message: "User created successfully", result})
     }catch(err){
-        res.status(500).json({message: err.message})
+        console.log(err)
     }
 }
+
+exports.signin = async (req, res) => {
+	const { email, password } = req.body;
+	try {
+		const { error, value } = signinSchema.validate({ email, password });
+		if (error) {
+			return res
+				.status(401)
+				.json({ success: false, message: error.details[0].message });
+		}
+
+		const existingUser = await User.findOne({ email }).select('+password');
+		if (!existingUser) {
+			return res
+				.status(401)
+				.json({ success: false, message: 'User does not exists!' });
+		}
+		const result = await doHashValidation(password, existingUser.password);
+		if (!result) {
+			return res
+				.status(401)
+				.json({ success: false, message: 'Invalid credentials!' });
+		}
+		const token = jwt.sign(
+			{
+				userId: existingUser._id,
+				email: existingUser.email,
+				verified: existingUser.verified,
+			},
+			process.env.TOKEN_SECRET,
+			{
+				expiresIn: '8h',
+			}
+		);
+
+		res
+			.cookie('Authorization', 'Bearer ' + token, {
+				expires: new Date(Date.now() + 8 * 3600000),
+				httpOnly: process.env.NODE_ENV === 'production',
+				secure: process.env.NODE_ENV === 'production',
+			})
+			.json({
+				success: true,
+				token,
+				message: 'logged in successfully',
+			});
+	} catch (error) {
+		console.log(error);
+	}
+};
