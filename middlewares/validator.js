a4d481a991aa7c29c72e017804d374b5037b90ec
mayrars <mayra203@gmail.com>
2024-11-14 07:08:42 -0600

    create validator for forgot password

--- middlewares/validator.js
+++ middlewares/validator.js
@@ -44,3 +44,17 @@ exports.changePasswordSchema = Joi.object({
 		.required()
 		.pattern(new RegExp('^(?=.*[a-z])(?=.*[A-Z])(?=.*d).{8,}$')),
 })
+
+exports.acceptFPCodeSchema = Joi.object({
+	email: Joi.string()
+		.min(6)
+		.max(60)
+		.required()
+		.email({
+			tlds: { allow: ['com', 'net'] },
+		}),
+	providedCode: Joi.number().required(),
+	newPassword: Joi.string()
+	.required()
+	.pattern(new RegExp('^(?=.*[a-z])(?=.*[A-Z])(?=.*d).{8,}$')),
+})
