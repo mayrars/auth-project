6c7f3d0a81f36eed2abf41cb409997bfef0be1c7
mayrars <mayra203@gmail.com>
2024-11-14 07:09:13 -0600

    create function for  verified and send password code

--- routers/authRouter.js
+++ routers/authRouter.js
@@ -10,5 +10,7 @@ router.post('/signout', identifier, authController.signout)
 router.patch('/send-verification-code', identifier, authController.sendVerificationCode)
 router.patch('/verify-verification-code', identifier, authController.verifyVerificationCode)
 router.patch('/change-password', identifier, authController.changePassword)
+router.patch('/send-forgot-password-code', identifier, authController.sendForgotPasswordCode)
+router.patch('/verify-forgot-password-code', identifier, authController.verifyForgotPasswordCode)
 
 module.exports = router
