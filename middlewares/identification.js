5462087a5e7d7f7a951ee0b82dfa62721a7dbff9
mayrars <mayra203@gmail.com>
2024-11-15 07:12:16 -0600

    change spaces

--- middlewares/identification.js
+++ middlewares/identification.js
@@ -1,25 +1,27 @@
-const jwt = require('jsonwebtoken')
-exports.identifier = (req, res, next) => {
-    let token
-    if(req.headers.client==='not-browser'){
-        token = req.headers.authorization
-    }else{
-        token = req.cookies.token
-    }
+const jwt = require('jsonwebtoken');
 
-    if(!token){
-        return res.status(403).json({success: false, message:'you are not authorized'})
-    }
-    try{
-        const userToken = token.split(' ')[1]
-        const jwtVertified = jwt.verify(userToken, process.env.TOKEN_SECRET)
-        if(jwtVertified){
-            req.user = jwtVertified
-            next()
-        }else{
-            throw new Error('you are not authorized')
-        }
-    }catch(error){
-        console.log(error)
-    }
-}
+exports.identifier = (req, res, next) => {
+	let token;
+	if (req.headers.client === 'not-browser') {
+		token = req.headers.authorization;
+	} else {
+		token = req.cookies['Authorization'];
+	}
+
+	if (!token) {
+		return res.status(403).json({ success: false, message: 'Unauthorized' });
+	}
+
+	try {
+		const userToken = token.split(' ')[1];
+		const jwtVerified = jwt.verify(userToken, process.env.TOKEN_SECRET);
+		if (jwtVerified) {
+			req.user = jwtVerified;
+			next();
+		} else {
+			throw new Error('error in the token');
+		}
+	} catch (error) {
+		console.log(error);
+	}
+};
