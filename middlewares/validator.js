0a64bbf6e0df0efcaf0fe57b5311d5152f4fbca6
mayrars <mayra203@gmail.com>
2024-11-18 20:33:50 -0600

    create post validator

--- middlewares/validator.js
+++ middlewares/validator.js
@@ -58,3 +58,16 @@ exports.acceptFPCodeSchema = Joi.object({
 	.required()
 	.pattern(new RegExp('^(?=.*[a-z])(?=.*[A-Z])(?=.*d).{8,}$')),
 })
+
+exports.createPostSchema = Joi.object({
+	title: Joi.string()
+		.min(6)
+		.max(60)
+		.required(),
+	description: Joi.string()
+		.min(6)
+		.max(600)
+		.required(),
+	userId: Joi.string().required(),
+	
+})
