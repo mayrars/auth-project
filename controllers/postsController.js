5e84a2e058d45462652d01d68dcff0364e76fadf
mayrars <mayra203@gmail.com>
2024-11-18 20:33:38 -0600

    create post function

--- controllers/postsController.js
+++ controllers/postsController.js
@@ -1,4 +1,4 @@
-const post = require('../models/postModel')
+const post = require('../models/postsModel')
 exports.getPosts = async(req, res) => {
     const {page} = req.query
     const postsPerPage = 10
@@ -9,7 +9,23 @@ exports.getPosts = async(req, res) => {
         }else{
             pageNum = page-1
         }
-        const result = await post.find().sort({createdAt: -1}).skip(pageNum*postsPerPage).limit(postsPerPage)
+        const result = await post.find().sort({createdAt: -1}).skip(pageNum*postsPerPage).limit(postsPerPage).populate({path: 'userId',select:'email'})
+        res.status(200).json({success: true, message: 'posts', data: result})
+    }catch(error){
+        console.log(error)
+    }
+}
+
+exports.createPost = async(req, res) => {
+    const {title, description} = req.body
+    const {userId} = req.user
+    try{
+        const {error, value} = createPostSchema.validate({
+            title,
+            description,
+            userId
+        })
+        
     }catch(error){
         console.log(error)
     }
