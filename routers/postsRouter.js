431b438886f18af15d9c27e41b4df8d331f42695
mayrars <mayra203@gmail.com>
2024-11-18 20:34:14 -0600

    Added create post

--- routers/postsRouter.js
+++ routers/postsRouter.js
@@ -4,9 +4,9 @@ const { identifier } = require('../middlewares/identification');
 const router = express.Router()
 
 router.get('/all-posts', postsController.getPosts)
+router.post('/create-post', identifier, postsController.createPost)
 /*
 router.get('/single-post', authController.signin)
-router.post('/create-post', identifier, authController.signout)
 
 router.put('/update-post', identifier, authController.sendVerificationCode)
 router.delete('/delete-post', identifier, authController.verifyVerificationCode)*/
